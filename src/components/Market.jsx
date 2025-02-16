bf367d753c6b65195ab4635b022fe483e3608755
Ayush kumar <kumarayush042004@gmail.com>
2025-02-16 18:16:47 +0530

    MarketPlace_inizialized

--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,5 +1,73 @@
 import React from 'react'
 
+
+const topProducts = [
+
+    {
+        id: 1,
+        name: "Ugaoo Organic Vermicompost Fertilizer Manure For Plants - 5 Kg",
+        category: "Fertilizers",
+        price: 399,
+        rating: 4.5,
+        image:"https://m.media-amazon.com/images/I/615ogT1U-KL.SL1044.jpg",
+        reviews: 100
+    },
+    {
+        id: 2,
+        name: "UGAOO Ridge Gourd Vegetable Seeds (Green, Pack of 2.4g)",
+        category: "Seed",
+        price: 299,
+        rating: 4.2,
+        image:"https://m.media-amazon.com/images/I/71X60AewLxL.SL1100.jpg",
+        reviews: 150
+    },
+    {
+        id: 3,
+        name: "rustBasket Gardening Hand Tools Set - 5 Pcs (Cultivator, Big and Small Trowel, Weeder, Fork)",
+        category: "Plants",
+        price: 349,
+        rating: 4.3,
+        image:"https://m.media-amazon.com/images/I/71cZQ9t3oNL._SL1500_.jpg",
+        reviews: 200
+    },
+    {
+        id: 4,
+        name: "Ugaoo Good Luck Jade Plant with Self Watering Pot",
+        category: "Plants",
+        price: 299,
+        rating: 4.2,
+        image:"https://m.media-amazon.com/images/I/71cZQ9t3oNL._SL1500_.jpg",
+        reviews: 150
+    },
+    {
+        id: 5,
+        name: "Ugaoo Areca Palm Plant with Self Watering Pot",
+        category: "Plants",
+        price: 349,
+        rating: 4.3,
+        image:"https://m.media-amazon.com/images/I/71cZQ9t3oNL._SL1500_.jpg",
+        reviews: 200
+    },
+    {
+        id: 6,
+        name: "Ugaoo Good Luck Jade Plant with Self Watering Pot",
+        category: "Plants",
+        price: 299,
+        rating: 4.2,
+        image:"https://m.media-amazon.com/images/I/71cZQ9t3oNL._SL1500_.jpg",
+        reviews: 150
+    },
+    {
+        id: 7,
+        name: "Ugaoo Areca Palm Plant with Self Watering Pot",
+        category: "Plants",
+        price: 349,
+        rating: 4.3,
+        image:"https://m.media-amazon.com/images/I/71cZQ9t3oNL._SL1500_.jpg",
+        reviews: 200
+    },
+]
+
 function Market() {
   return (
     <div>Market</div>
